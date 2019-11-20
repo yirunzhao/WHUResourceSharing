6806016d715d5c90dfb15575cd3584677b13496c
Chenaifeiwu <651844925@qq.com>
2019-11-20 13:05:35 +0800

    test2

--- WHUResourceSharing/urls.py
+++ WHUResourceSharing/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from apps.base import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cms/',include('apps.cms.urls')),
+    path('',views.login_view,name='main_login'),
 ]
